dbc7b735d90964597f4efdb75758725a8a7f4b60
deepakQE <84173190+deepakQE@users.noreply.github.com>
2024-04-09 14:34:27 +0530

    Update Calculator.py

--- Task 01 Calculator/Calculator.py
+++ Task 01 Calculator/Calculator.py
@@ -1,1 +1,23 @@
-
+while True:
+    X = input("Enter 'Yes' if you want to do more calculations and 'No' to exit: ")
+    if X == "Yes" or "yes":
+        a = input("Enter the operation to be performed (+, -, /, *, %): ")
+        b = int(input("Enter the first number: "))
+        c = int(input("Enter the second number: "))
+        if a == '+':
+            d = b + c
+        elif a == '-':
+            d = b - c
+        elif a == '/':
+            d = b / c
+        elif a == '*':
+            d = b * c
+        elif a == '%':
+            d = b % c
+        else:
+            print("Invalid operation. Please retry.")
+            continue
+        print("Your result is:", d)
+    else:
+        break
+    
