67353582c0733364695878b1cdaf984a3b2c58ca
deepakQE <84173190+deepakQE@users.noreply.github.com>
2024-04-10 18:40:15 +0530

    Update README.py

--- Task 03 Rock-Paper-Scissors Game/README.py
+++ Task 03 Rock-Paper-Scissors Game/README.py
@@ -1,1 +1,11 @@
+# Rock-Paper-Scissors Game
+
+* The game is played by the User and Computer
+
+* Condition Used
+  * while loop
+  * if else condition
+  * continue
+  * break
+# The program is good for a Practice purpose
 
